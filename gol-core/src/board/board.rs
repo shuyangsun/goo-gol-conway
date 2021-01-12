aed1c6691c8fdfd2cbdc75947e2416a4902d7b11
Shuyang Sun <sunbuffett@gmail.com>
2021-01-12 10:24:40 -0500

    Add advance implementation in board.

--- gol-core/src/board/board.rs
+++ gol-core/src/board/board.rs
@@ -1,9 +1,25 @@
+use super::super::cell::cell::Cell;
+use itertools::izip;
+
 pub trait Board<T> {
-    fn cell_state(&self, idx: usize) -> T;
+    fn cell_state(&self, idx: usize) -> &T;
     fn set_cell_state(&mut self, idx: usize, new_state: T);
-    fn get_cell_neighbors(&self, idx: usize) -> Box<&dyn Iterator<Item = usize>>;
+    fn get_cell_neighbors_states(&self, idx: usize) -> Box<dyn Iterator<Item = (usize, &T)>>;
+    fn cell_iter(&self) -> Box<dyn Iterator<Item = &dyn Cell<T>>>;
+    fn cell_iter_mut(&self) -> Box<dyn Iterator<Item = &mut dyn Cell<T>>>;
 
     fn advance(&mut self) {
-        // TODO
+        let mut new_states: Vec<T> = Vec::new();
+        for (idx, cell) in self.cell_iter().enumerate() {
+            let neighbors = self.get_cell_neighbors_states(idx);
+            let new_state = cell.next_state(&*neighbors);
+            new_states.push(new_state);
+        }
+
+        // TODO: call back and set states.
+
+        for (cell, new_state) in izip!(self.cell_iter_mut(), new_states) {
+            cell.set_state(new_state);
+        }
     }
 }
--- gol-core/src/cell/cell.rs
+++ gol-core/src/cell/cell.rs
@@ -1,5 +1,8 @@
 pub trait Cell<T> {
+    fn new_from_state(state: T) -> Self
+    where
+        Self: Sized;
     fn state(&self) -> &T;
     fn set_state(&mut self, new_state: T);
-    fn next_state(&self, neighbors: &dyn Iterator<Item = (usize, T)>) -> T;
+    fn next_state(&self, neighbors: &dyn Iterator<Item = (usize, &T)>) -> T;
 }
