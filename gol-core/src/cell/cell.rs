49adfaa24617c9ee61628bda6860dfb83e0d8981
Shuyang Sun <sunbuffett@gmail.com>
2021-01-13 10:08:50 -0500

    Remove redundant import.

--- gol-core/src/cell/cell.rs
+++ gol-core/src/cell/cell.rs
@@ -1,10 +1,7 @@
-use super::super::evolution::strategy::EvolutionStrategy;
-
 pub trait Cell<T>: Sync + Send {
     fn new_from_state(state: T) -> Self
     where
         Self: Sized;
     fn state(&self) -> &T;
     fn set_state(&mut self, new_state: T);
-    //    fn evolution_strategy(&self) -> &dyn EvolutionStrategy<T>;
 }
