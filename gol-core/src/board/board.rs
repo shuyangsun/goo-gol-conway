2d5fae89c0a74ca216bd0b8daa91ae990c7719bf
Shuyang Sun <sunbuffett@gmail.com>
2021-01-13 21:44:24 -0500

    Implementing advance.

--- gol-core/src/board/board.rs
+++ gol-core/src/board/board.rs
@@ -1,8 +1,8 @@
-use super::super::cell::common::IndexedDataOwned;
+use super::super::cell::common::{CellIndex, CellState, IndexedDataOwned, IndexedDataRef};
 use super::super::evolution::strategy::EvolutionStrategy;
 use rayon::prelude::*;
 
-pub trait BoardStateManager<'data, 'dref, T, CI, I1, I2, I3>
+pub trait BoardStateManager<'data, 'dref, T, CI, I1, I2, I3>: Send + Sync
 where
     'data: 'dref,
     T: 'data + Send + Sync,
@@ -16,51 +16,64 @@ where
     fn update_cell_states_from_par_iter(&mut self, new_states: I3);
 }
 
-pub trait BoardSpaceManager<CI, I1, I2>
+pub trait BoardSpaceManager<CI, I1, I2>: Send + Sync
 where
     CI: Send + Sync,
     I1: Iterator<Item = CI>,
     I2: ParallelIterator<Item = CI>,
 {
-    fn convert_from_linear_index(&self, linear_idx: usize) -> CI;
-    fn convert_to_linear_index(&self, idx: CI) -> usize;
-
     fn indices_iter(&self) -> I1;
+    fn indices_par_iter(&self) -> I2;
     fn get_neighbors_idx(&self, idx: CI) -> I1;
 }
 
-pub trait BoardStrategyManager<'data, 'dref, CI, T, I> {
+pub trait BoardStrategyManager<'data, 'dref, CI, T, I>: Send + Sync {
     fn get_strategy_at_index(&self, idx: CI) -> &dyn EvolutionStrategy<'data, 'dref, CI, T, I>;
 }
 
-// pub trait Board<'a, 'b, T, I1, I2, I3>: Send + Sync
-// where
-//     'a: 'b,
-//     T: 'a + Send + Sync + Clone,
-//     I1: IndexedParallelIterator<Item = &'b T>,
-//     I2: IndexedParallelIterator<Item = &'b mut T>,
-//     I3: IndexedParallelIterator<Item = &'b dyn EvolutionStrategy<'a, T>>,
-// {
-//     fn data(&self) -> &dyn BoardData<'a, 'b, T, I1, I2, I3>;
-//     fn advance(&mut self) {
-//         let c_iter: I1 = self.data().cell_states_par_iter();
-//         let strategy_iter: I3 = self.data().cell_strategy_par_iter_mut();
-//         let new_states: Vec<T> = c_iter
-//             .zip(strategy_iter)
-//             .enumerate()
-//             .map(
-//                 |(idx, (state, strat)): (usize, (&'b T, &dyn EvolutionStrategy<T>))| {
-//                     let neighbors = Box::new(self.data().get_cell_neighbor_states(idx));
-//                     strat.next_state(idx, state, neighbors)
-//                 },
-//             )
-//             .collect();
-//
-//         // TODO: call back and set states.
-//
-//         let c_iter_mut: I2 = self.data().cell_states_par_iter_mut();
-//         c_iter_mut
-//             .zip(new_states.par_iter())
-//             .for_each(|(old_state, new_state)| *old_state = (*new_state).clone());
-//     }
-// }
+pub trait Board<'data, 'dref, T, CI, I1, I2, I3, I4>
+where
+    'data: 'dref,
+    T: 'data + CellState,
+    CI: CellIndex,
+    I1: Iterator<Item = CI>,
+    I2: Iterator<Item = IndexedDataOwned<CI, T>>,
+    I3: ParallelIterator<Item = IndexedDataOwned<CI, T>>,
+    I4: ParallelIterator<Item = CI>,
+{
+    fn state_manager(&self) -> &dyn BoardStateManager<'data, 'dref, T, CI, I1, I2, I3>;
+    fn state_manager_mut(&mut self) -> &mut dyn BoardStateManager<'data, 'dref, T, CI, I1, I2, I3>;
+    fn space_manager(&self) -> &dyn BoardSpaceManager<CI, I1, I4>;
+    fn strategy_manager(
+        &self,
+    ) -> &dyn BoardStrategyManager<
+        'data,
+        'dref,
+        CI,
+        T,
+        std::vec::IntoIter<IndexedDataRef<'dref, CI, T>>,
+    >;
+
+    fn iter_count(&self) -> usize;
+
+    fn advance(&mut self) {
+        let states = self.state_manager();
+        let strat = self.strategy_manager();
+        let space = self.space_manager();
+        let next_states = self.space_manager().indices_par_iter().map(|idx| {
+            let cur_state = states.get_cell_state(idx);
+            let neighbors: Vec<IndexedDataRef<'dref, CI, T>> = space
+                .get_neighbors_idx(idx)
+                .map(|neighbor_idx| (idx, states.get_cell_state(neighbor_idx)))
+                .collect();
+            strat
+                .get_strategy_at_index(idx)
+                .next_state(idx, cur_state, neighbors.into_iter())
+        });
+
+        // TODO: Drawing and callbacks.
+
+        self.state_manager_mut()
+            .update_cell_states_from_par_iter(next_states.par_iter());
+    }
+}
--- gol-core/src/cell/state.rs
+++ gol-core/src/cell/state.rs
@@ -1,7 +1,3 @@
-pub trait CellState: Clone + Send + Sync {}
-
-impl<T> CellState for T where T: Clone + Send + Sync {}
-
 #[derive(Debug, Clone, PartialEq)]
 pub enum ConwayState {
     Alive,
