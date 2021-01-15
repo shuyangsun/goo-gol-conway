cda0b0b98c696ef6dc33b44db916a9446b1af787
Shuyang Sun <sunbuffett@gmail.com>
2021-01-15 12:12:18 -0500

    Add templates for neighbor implementation.

--- gol-core/src/neighbors/grid_surround.rs
+++ gol-core/src/neighbors/grid_surround.rs
@@ -1,1 +1,44 @@
+use crate::{BoardNeighborManager, GridPoint1D, GridPoint2D, GridPoint3D, GridPointND};
+use num_traits::PrimInt;
+
 pub struct NeighborsGridSurroud {}
+
+impl<T> BoardNeighborManager<GridPointND<T>, std::vec::IntoIter<GridPointND<T>>>
+    for NeighborsGridSurroud
+where
+    T: Send + Sync + PrimInt,
+{
+    fn get_neighbors_idx(&self, idx: GridPointND<T>) -> std::vec::IntoIter<GridPointND<T>> {
+        Vec::new().into_iter() // TODO
+    }
+}
+
+impl<T> BoardNeighborManager<GridPoint3D<T>, std::vec::IntoIter<GridPoint3D<T>>>
+    for NeighborsGridSurroud
+where
+    T: Send + Sync + PrimInt,
+{
+    fn get_neighbors_idx(&self, idx: GridPoint3D<T>) -> std::vec::IntoIter<GridPoint3D<T>> {
+        Vec::new().into_iter() // TODO
+    }
+}
+
+impl<T> BoardNeighborManager<GridPoint2D<T>, std::vec::IntoIter<GridPoint2D<T>>>
+    for NeighborsGridSurroud
+where
+    T: Send + Sync + PrimInt,
+{
+    fn get_neighbors_idx(&self, idx: GridPoint2D<T>) -> std::vec::IntoIter<GridPoint2D<T>> {
+        Vec::new().into_iter() // TODO
+    }
+}
+
+impl<T> BoardNeighborManager<GridPoint1D<T>, std::vec::IntoIter<GridPoint1D<T>>>
+    for NeighborsGridSurroud
+where
+    T: Send + Sync + PrimInt,
+{
+    fn get_neighbors_idx(&self, idx: GridPoint1D<T>) -> std::vec::IntoIter<GridPoint1D<T>> {
+        Vec::new().into_iter() // TODO
+    }
+}
