3be1832d99e4ca62eaeab3bdc88bb06828dcec6f
Shuyang Sun <sunbuffett@gmail.com>
2021-01-15 01:59:23 -0500

    Remove copy requirement on states.

--- gol-core/src/cell/common.rs
+++ gol-core/src/cell/common.rs
@@ -1,8 +1,8 @@
-pub trait CellState: Clone + Copy + Send + Sync {}
+pub trait CellState: Clone + Send + Sync {}
 pub trait CellIndex: Clone + Send + Sync {}
 
 pub type IndexedDataRef<'dref, CI, T> = (CI, &'dref T);
 pub type IndexedDataOwned<CI, T> = (CI, T);
 
-impl<T> CellState for T where T: Clone + Copy + Send + Sync {}
+impl<T> CellState for T where T: Clone + Send + Sync {}
 impl<T> CellIndex for T where T: Clone + Send + Sync {}
--- gol-core/src/cell/index.rs
+++ gol-core/src/cell/index.rs
@@ -1,8 +1,8 @@
-#[derive(Clone, Copy)]
+#[derive(Clone)]
 pub struct GridPoint1D(i64);
-#[derive(Clone, Copy)]
+#[derive(Clone)]
 pub struct GridPoint2D(i64, i64);
-#[derive(Clone, Copy)]
+#[derive(Clone)]
 pub struct GridPoint3D(i64, i64, i64);
 #[derive(Clone)]
 pub struct GridPointND(Vec<i64>);
