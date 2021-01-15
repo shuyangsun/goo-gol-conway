9a3cf202e172e1e6207a6f6b9bcaa86765c3546d
Shuyang Sun <sunbuffett@gmail.com>
2021-01-15 10:50:32 -0500

    Add new grid initialization with origin method.

--- gol-core/src/space/grid.rs
+++ gol-core/src/space/grid.rs
@@ -2,6 +2,11 @@ use crate::{BoardSpaceManager, GridPoint1D, GridPoint2D, GridPoint3D, GridPointN
 use num_traits::{CheckedDiv, FromPrimitive, PrimInt, ToPrimitive, Unsigned};
 use rayon::prelude::*;
 
+pub enum GridOrigin {
+    Center,
+    Zero,
+}
+
 pub struct GridND<T> {
     indices: Vec<T>,
 }
@@ -26,12 +31,21 @@ impl<T> GridND<GridPointND<T>> {
         U: PrimInt + Unsigned + ToPrimitive + FromPrimitive + Send + Sync,
         I: Iterator<Item = U>,
     {
+        Self::new_with_origin(shape, GridOrigin::Center)
+    }
+
+    pub fn new_with_origin<U, I>(shape: I, origin: GridOrigin) -> Self
+    where
+        T: PrimInt + CheckedDiv + std::convert::TryFrom<U> + Send + Sync,
+        U: PrimInt + Unsigned + ToPrimitive + FromPrimitive + Send + Sync,
+        I: Iterator<Item = U>,
+    {
         let shape_vec: Vec<U> = shape.collect();
-        let indices = Self::indices_vec(&shape_vec);
+        let indices = Self::indices_vec(&shape_vec, origin);
         Self { indices }
     }
 
-    fn indices_vec<U>(shape: &Vec<U>) -> Vec<GridPointND<T>>
+    fn indices_vec<U>(shape: &Vec<U>, origin: GridOrigin) -> Vec<GridPointND<T>>
     where
         T: PrimInt + CheckedDiv + std::convert::TryFrom<U> + Send + Sync,
         U: PrimInt + Unsigned + ToPrimitive + FromPrimitive + Send + Sync,
@@ -40,6 +54,8 @@ impl<T> GridND<GridPointND<T>> {
         for dim in shape.iter() {
             num_cell = num_cell * *dim;
         }
+        let zero_t = T::zero();
+        let two_t = T::one() + T::one();
         (0..num_cell.to_u64().unwrap())
             .into_par_iter()
             .map(|i| {
@@ -47,11 +63,19 @@ impl<T> GridND<GridPointND<T>> {
                 let mut res = Vec::new();
                 let mut cur = i;
                 for dim in shape.iter() {
-                    let dim_idx_raw = cur / *dim;
-                    let dim_idx = match T::try_from(dim_idx_raw) {
+                    let dim_t = match T::try_from(*dim) {
                         Ok(val) => val,
                         Err(_) => panic!("Cannot convert size to index type."),
                     };
+                    let cur_t = match T::try_from(cur) {
+                        Ok(val) => val,
+                        Err(_) => panic!("Cannot convert size to index type."),
+                    };
+                    let dim_idx = cur_t / dim_t
+                        - match &origin {
+                            GridOrigin::Center => dim_t / two_t,
+                            GridOrigin::Zero => zero_t,
+                        };
                     res.push(dim_idx);
                     cur = cur % *dim;
                 }
@@ -120,12 +144,14 @@ impl<T> GridND<GridPoint1D<T>> {
 
 #[cfg(test)]
 mod grid_tests {
-    use crate::{BoardSpaceManager, GridND, GridPoint1D, GridPoint2D, GridPoint3D, GridPointND};
+    use crate::{
+        BoardSpaceManager, GridND, GridOrigin, GridPoint1D, GridPoint2D, GridPoint3D, GridPointND,
+    };
     use rayon::prelude::*;
 
     #[test]
     fn grid_1d_test_1() {
-        type Point = GridPoint1D<u64>;
+        type Point = GridPoint1D<i32>;
 
         let grid = Box::new(GridND::<Point>::new(10u64))
             as Box<
@@ -161,7 +187,7 @@ mod grid_tests {
 
     #[test]
     fn grid_2d_test_1() {
-        type Point = GridPoint2D<u64>;
+        type Point = GridPoint2D<i64>;
 
         let grid = Box::new(GridND::<Point>::new(5u64, 10))
             as Box<
@@ -179,7 +205,7 @@ mod grid_tests {
 
     #[test]
     fn grid_3d_test_1() {
-        type Point = GridPoint3D<u64>;
+        type Point = GridPoint3D<i32>;
 
         let grid = Box::new(GridND::<Point>::new(5u64, 10, 6))
             as Box<
@@ -197,7 +223,7 @@ mod grid_tests {
 
     #[test]
     fn grid_nd_test_1() {
-        type Point = GridPointND<u64>;
+        type Point = GridPointND<i32>;
 
         let grid = Box::new(GridND::<Point>::new(vec![5u64, 10, 6, 10].into_iter()))
             as Box<
@@ -211,5 +237,39 @@ mod grid_tests {
         let indices_par: Vec<Point> = grid.indices_par_iter().collect();
         assert_eq!(indices.len(), 3000);
         assert_eq!(indices_par.len(), indices.len());
+    }
+
+    #[test]
+    fn grid_nd_test_2() {
+        type Point = GridPointND<i32>;
+        let board_size = vec![2u32, 2, 2, 2, 2];
+
+        let grid_1 = Box::new(GridND::<Point>::new(board_size.clone().into_iter()))
+            as Box<
+                dyn BoardSpaceManager<
+                    Point,
+                    std::vec::IntoIter<Point>,
+                    rayon::vec::IntoIter<Point>,
+                >,
+            >;
+        let grid_2 = Box::new(GridND::<Point>::new_with_origin(
+            board_size.into_iter(),
+            GridOrigin::Zero,
+        ))
+            as Box<
+                dyn BoardSpaceManager<
+                    Point,
+                    std::vec::IntoIter<Point>,
+                    rayon::vec::IntoIter<Point>,
+                >,
+            >;
+        let indices_1: Vec<Point> = grid_1.indices_iter().collect();
+        let indices_par_1: Vec<Point> = grid_1.indices_par_iter().collect();
+        let indices_2: Vec<Point> = grid_2.indices_iter().collect();
+        let indices_par_2: Vec<Point> = grid_2.indices_par_iter().collect();
+        assert_eq!(indices_1.len(), 32);
+        assert_eq!(indices_par_1.len(), indices_1.len());
+        assert_eq!(indices_2.len(), indices_1.len());
+        assert_eq!(indices_par_2.len(), indices_1.len());
     }
 }
