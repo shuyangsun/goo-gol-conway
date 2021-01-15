718df24d50cb668deb5f4f3424e3a197242fd0e5
Shuyang Sun <sunbuffett@gmail.com>
2021-01-15 00:04:36 -0500

    Using threads to implement callback.

--- gol-client/src/main.rs
+++ gol-client/src/main.rs
@@ -1,3 +1,17 @@
+use std::thread;
+use std::time::Duration;
+
 fn main() {
-    println!("Hello, world!");
+    let handle = thread::spawn(|| {
+        for i in 1..10 {
+            println!("*** {}", i);
+            thread::sleep(Duration::from_millis(1));
+        }
+    });
+
+    for i in 1..5 {
+        println!("{}", i);
+        thread::sleep(Duration::from_millis(1));
+    }
+    handle.join().unwrap();
 }
--- gol-core/src/board/board_callback.rs
+++ gol-core/src/board/board_callback.rs
@@ -1,5 +1,7 @@
 use crate::{CellIndex, CellState, IndexedDataOwned};
 use rayon::prelude::*;
+use std::sync::{Arc, Mutex};
+use std::thread::{self, JoinHandle};
 
 pub trait BoardCallback<T, CI, I>: Send + Sync
 where
@@ -16,15 +18,15 @@ where
     CI: Send + Sync,
     I: ParallelIterator<Item = IndexedDataOwned<CI, T>>,
 {
-    callbacks: Vec<Box<dyn BoardCallback<T, CI, I>>>,
+    callbacks: Vec<Arc<Mutex<Box<dyn BoardCallback<T, CI, I>>>>>,
     states_cache: Option<Vec<IndexedDataOwned<CI, T>>>,
-    futures_res: Vec<usize>,
+    callback_handle: Arc<Mutex<Option<JoinHandle<()>>>>,
 }
 
 impl<T, CI> BoardCallbackManager<T, CI, rayon::vec::IntoIter<IndexedDataOwned<CI, T>>>
 where
-    T: CellState,
-    CI: CellIndex,
+    T: 'static + CellState,
+    CI: 'static + CellIndex,
 {
     pub fn new(
         callbacks: Vec<
@@ -32,23 +34,43 @@ where
         >,
     ) -> Self {
         Self {
-            callbacks,
+            callbacks: callbacks
+                .into_iter()
+                .map(|ele| Arc::new(Mutex::new(ele)))
+                .collect(),
             states_cache: None,
-            futures_res: Vec::new(),
+            callback_handle: Arc::new(Mutex::new(None)),
         }
     }
 
-    //     pub async fn call(&self, next_states: Vec<IndexedDataOwned<CI, T>>)
-    //     where
-    //         CI: 'static,
-    //         T: 'static,
-    //     {
-    //         let asdf: tokio::task::JoinHandle<()> =
-    //             tokio::spawn(async { self.callbacks[0].execute(next_states.into_par_iter()) });
-    //     }
-    //
-    //     pub fn block_until_finish(&mut self) {
-    //         // TODO: implementation
-    //         self.futures_res = Vec::new();
-    //     }
+    pub fn call(&self, next_states: Vec<IndexedDataOwned<CI, T>>) {
+        self.block_until_finish();
+        debug_assert!(self.callback_handle.lock().unwrap().is_none());
+
+        let callbacks: Vec<
+            Arc<
+                Mutex<Box<dyn BoardCallback<T, CI, rayon::vec::IntoIter<IndexedDataOwned<CI, T>>>>>,
+            >,
+        > = self.callbacks.iter().map(|ele| Arc::clone(&ele)).collect();
+
+        let mut handle = self.callback_handle.lock().unwrap();
+        *handle = Some(thread::spawn(|| {
+            callbacks.par_iter().for_each(|ele| {
+                ele.lock()
+                    .unwrap()
+                    .execute(next_states.clone().into_par_iter())
+            });
+        }));
+    }
+
+    pub fn block_until_finish(&self) {
+        self.callback_handle
+            .lock()
+            .unwrap()
+            .unwrap()
+            .join()
+            .unwrap();
+        let mut handle = self.callback_handle.lock().unwrap();
+        *handle = None;
+    }
 }
