8c57d356691a55afca53c5614a0d9b6e91c248bf
Shuyang Sun <sunbuffett@gmail.com>
2021-01-16 08:32:21 -0500

    Implement helper methods on index.

--- gol-core/src/cell/index.rs
+++ gol-core/src/cell/index.rs
@@ -1,3 +1,7 @@
+pub trait ToGridPointND<T> {
+    fn to_nd(&self) -> GridPointND<T>;
+}
+
 #[derive(Clone)]
 pub struct GridPoint1D<T> {
     pub x: T,
@@ -19,4 +23,45 @@ pub struct GridPoint3D<T> {
 #[derive(Clone)]
 pub struct GridPointND<T> {
     pub idx: Vec<T>,
+}
+
+impl<T> ToGridPointND<T> for GridPoint1D<T> {
+    fn to_nd(&self) -> GridPointND<T> {
+        GridPointND{ idx: vec![self.x] }
+    }
+}
+
+impl<T> ToGridPointND<T> for GridPoint2D<T> {
+    fn to_nd(&self) -> GridPointND<T> {
+        GridPointND{ idx: vec![self.x, self.y] }
+    }
+}
+
+impl<T> ToGridPointND<T> for GridPoint3D<T> {
+    fn to_nd(&self) -> GridPointND<T> {
+        GridPointND{ idx: vec![self.x, self.y, self.z] }
+    }
+}
+
+impl<T> GridPointND<T> {
+    pub fn to_1d(&self) -> Option<GridPoint1D<T>>  {
+        match self.idx.len() {
+            1 => Some(GridPoint1D { x: self.idx[0] }),
+            _ => None
+        }
+    }
+
+    pub fn to_2d(&self) -> Option<GridPoint2D<T>> {
+        match self.idx.len() {
+            2 => Some(GridPoint2D { x: self.idx[0], y: self.idx[1] }),
+            _ => None
+        }
+    }
+
+    pub fn to_3d(&self) -> Option<GridPoint3D<T>> {
+        match self.idx.len() {
+            3 => Some(GridPoint3D { x: self.idx[0], y: self.idx[1], z: self.idx[2] }),
+            _ => None
+        }
+    }
 }
