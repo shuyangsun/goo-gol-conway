14880a24674beb831e2c28fb47d6d7d2a42ae9cb
Shuyang Sun <sunbuffett@gmail.com>
2021-01-17 14:19:32 -0500

    Add gol-core and gol-renderer dependency to gol-client.

--- gol-client/src/main.rs
+++ gol-client/src/main.rs
@@ -1,18 +1,1 @@
-use std::thread;
-use std::time::Duration;
-
-/// This is mainly just for concurrency/parallelism practice for now.
-fn main() {
-    let handle = thread::spawn(|| {
-        for i in 1..10 {
-            println!("*** {}", i);
-            thread::sleep(Duration::from_millis(1));
-        }
-    });
-
-    for i in 1..5 {
-        println!("{}", i);
-        thread::sleep(Duration::from_millis(1));
-    }
-    handle.join().unwrap();
-}
+fn main() {}
