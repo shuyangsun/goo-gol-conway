27eebaa600762b3423b6eaff0e86708e5a9f8333
Shuyang Sun <sunbuffett@gmail.com>
2021-01-17 13:44:01 -0500

    Finish implementing standard board factories.

--- gol-core/src/board/standard.rs
+++ gol-core/src/board/standard.rs
@@ -1,8 +1,9 @@
 use crate::neighbors::grid_surround::{MarginPrimInt, PointPrimInt};
 use crate::{
     Board, BoardCallback, BoardCallbackManager, BoardNeighborManager, BoardSpaceManager,
-    BoardStateManager, BoardStrategyManager, EvolutionStrategy, Grid, GridFactory, GridPointND,
-    IndexedDataOwned, NeighborsGridSurround, SharedStrategyManager, SparseStates,
+    BoardStateManager, BoardStrategyManager, EvolutionStrategy, Grid, GridFactory, GridPoint1D,
+    GridPoint2D, GridPoint3D, GridPointND, IndexedDataOwned, NeighborsGridSurround,
+    SharedStrategyManager, SparseStates,
 };
 use num_traits::{CheckedDiv, FromPrimitive, PrimInt, Unsigned};
 use rayon;
@@ -94,10 +95,10 @@ where
 }
 
 impl StandardBoardFactory {
-    pub fn new_standard_nd_grid<T, U, K, I>(
+    pub fn new_standard_nd_grid<T, U, S, I>(
         shape: I,
-        default_state: &T,
-        neighbor_margin: &K,
+        default_state: T,
+        neighbor_margin: S,
         initial_states: &HashMap<GridPointND<U>, T>,
         strategy: Box<
             dyn EvolutionStrategy<
@@ -118,13 +119,137 @@ impl StandardBoardFactory {
     ) -> StandardBoard<T, GridPointND<U>, std::vec::IntoIter<GridPointND<U>>>
     where
         T: 'static + Send + Sync + Clone + PartialEq,
-        U: 'static + Hash + PrimInt + CheckedDiv + std::convert::TryFrom<K> + PointPrimInt,
-        K: 'static + Unsigned + FromPrimitive + MarginPrimInt,
-        I: Iterator<Item = K>,
+        U: 'static + Hash + PrimInt + CheckedDiv + std::convert::TryFrom<S> + PointPrimInt,
+        S: 'static + Unsigned + FromPrimitive + MarginPrimInt,
+        I: Iterator<Item = S>,
     {
         let space_manager = Grid::<GridPointND<U>>::new(shape);
-        let neighbor_manager = NeighborsGridSurround::new(*neighbor_margin);
-        let state_manager = SparseStates::new(default_state.clone(), initial_states);
+        let neighbor_manager = NeighborsGridSurround::new(neighbor_margin);
+        let state_manager = SparseStates::new(default_state, initial_states);
+        let strategy_manger = SharedStrategyManager::new(strategy);
+        StandardBoard::new(
+            Box::new(space_manager),
+            Box::new(neighbor_manager),
+            Box::new(state_manager),
+            Box::new(strategy_manger),
+            callbacks,
+        )
+    }
+
+    pub fn new_standard_3d_grid<T, U, S, I>(
+        shape: (S, S, S),
+        default_state: T,
+        neighbor_margin: S,
+        initial_states: &HashMap<GridPoint3D<U>, T>,
+        strategy: Box<
+            dyn EvolutionStrategy<
+                GridPoint3D<U>,
+                T,
+                std::vec::IntoIter<IndexedDataOwned<GridPoint3D<U>, T>>,
+            >,
+        >,
+        callbacks: Vec<
+            Box<
+                dyn BoardCallback<
+                    T,
+                    GridPoint3D<U>,
+                    rayon::vec::IntoIter<IndexedDataOwned<GridPoint3D<U>, T>>,
+                >,
+            >,
+        >,
+    ) -> StandardBoard<T, GridPoint3D<U>, std::vec::IntoIter<GridPoint3D<U>>>
+    where
+        T: 'static + Send + Sync + Clone + PartialEq,
+        U: 'static + Hash + PrimInt + CheckedDiv + std::convert::TryFrom<S> + PointPrimInt,
+        S: 'static + Unsigned + FromPrimitive + MarginPrimInt,
+        I: Iterator<Item = S>,
+    {
+        let space_manager =
+            Grid::<GridPoint3D<U>>::new(vec![shape.0, shape.1, shape.2].into_iter());
+        let neighbor_manager = NeighborsGridSurround::new(neighbor_margin);
+        let state_manager = SparseStates::new(default_state, initial_states);
+        let strategy_manger = SharedStrategyManager::new(strategy);
+        StandardBoard::new(
+            Box::new(space_manager),
+            Box::new(neighbor_manager),
+            Box::new(state_manager),
+            Box::new(strategy_manger),
+            callbacks,
+        )
+    }
+
+    pub fn new_standard_2d_grid<T, U, S, I>(
+        shape: (S, S),
+        default_state: T,
+        neighbor_margin: S,
+        initial_states: &HashMap<GridPoint2D<U>, T>,
+        strategy: Box<
+            dyn EvolutionStrategy<
+                GridPoint2D<U>,
+                T,
+                std::vec::IntoIter<IndexedDataOwned<GridPoint2D<U>, T>>,
+            >,
+        >,
+        callbacks: Vec<
+            Box<
+                dyn BoardCallback<
+                    T,
+                    GridPoint2D<U>,
+                    rayon::vec::IntoIter<IndexedDataOwned<GridPoint2D<U>, T>>,
+                >,
+            >,
+        >,
+    ) -> StandardBoard<T, GridPoint2D<U>, std::vec::IntoIter<GridPoint2D<U>>>
+    where
+        T: 'static + Send + Sync + Clone + PartialEq,
+        U: 'static + Hash + PrimInt + CheckedDiv + std::convert::TryFrom<S> + PointPrimInt,
+        S: 'static + Unsigned + FromPrimitive + MarginPrimInt,
+        I: Iterator<Item = S>,
+    {
+        let space_manager = Grid::<GridPoint2D<U>>::new(vec![shape.0, shape.1].into_iter());
+        let neighbor_manager = NeighborsGridSurround::new(neighbor_margin);
+        let state_manager = SparseStates::new(default_state, initial_states);
+        let strategy_manger = SharedStrategyManager::new(strategy);
+        StandardBoard::new(
+            Box::new(space_manager),
+            Box::new(neighbor_manager),
+            Box::new(state_manager),
+            Box::new(strategy_manger),
+            callbacks,
+        )
+    }
+
+    pub fn new_standard_1d_grid<T, U, S, I>(
+        shape: (S, S),
+        default_state: T,
+        neighbor_margin: S,
+        initial_states: &HashMap<GridPoint1D<U>, T>,
+        strategy: Box<
+            dyn EvolutionStrategy<
+                GridPoint1D<U>,
+                T,
+                std::vec::IntoIter<IndexedDataOwned<GridPoint1D<U>, T>>,
+            >,
+        >,
+        callbacks: Vec<
+            Box<
+                dyn BoardCallback<
+                    T,
+                    GridPoint1D<U>,
+                    rayon::vec::IntoIter<IndexedDataOwned<GridPoint1D<U>, T>>,
+                >,
+            >,
+        >,
+    ) -> StandardBoard<T, GridPoint1D<U>, std::vec::IntoIter<GridPoint1D<U>>>
+    where
+        T: 'static + Send + Sync + Clone + PartialEq,
+        U: 'static + Hash + PrimInt + CheckedDiv + std::convert::TryFrom<S> + PointPrimInt,
+        S: 'static + Unsigned + FromPrimitive + MarginPrimInt,
+        I: Iterator<Item = S>,
+    {
+        let space_manager = Grid::<GridPoint1D<U>>::new(vec![shape.0, shape.1].into_iter());
+        let neighbor_manager = NeighborsGridSurround::new(neighbor_margin);
+        let state_manager = SparseStates::new(default_state, initial_states);
         let strategy_manger = SharedStrategyManager::new(strategy);
         StandardBoard::new(
             Box::new(space_manager),
