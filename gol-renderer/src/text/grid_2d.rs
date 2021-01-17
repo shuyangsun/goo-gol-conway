4f51d5335128f9f4a41fc9e6926840f401965230
Shuyang Sun <sunbuffett@gmail.com>
2021-01-17 14:17:45 -0500

    Test text renderer.

--- gol-renderer/src/text/grid_2d.rs
+++ gol-renderer/src/text/grid_2d.rs
@@ -1,3 +1,24 @@
 use gol_core::{BoardCallback, GridPoint2D, IndexedDataOwned};
+use rayon::prelude::*;
 
-pub struct TextRendererGrid2D {}
+pub struct TextRendererGrid2D {
+    has_cell_boarder: bool,
+}
+
+impl<T, U, I> BoardCallback<T, GridPoint2D<U>, I> for TextRendererGrid2D
+where
+    T: Send + Sync + Clone + std::convert::Into<char>,
+    U: Send + Sync,
+    I: ParallelIterator<Item = IndexedDataOwned<GridPoint2D<U>, T>>,
+{
+    fn execute(&self, states: I) {
+        let states: Vec<IndexedDataOwned<GridPoint2D<U>, T>> = states.collect();
+        println!("first state: {}", states.first().unwrap().1.clone().into());
+    }
+}
+
+impl TextRendererGrid2D {
+    pub fn new(has_cell_boarder: bool) -> Self {
+        Self { has_cell_boarder }
+    }
+}
