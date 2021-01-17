f1d4439cbc6ee0eb0c8ba94fc8cb9716ccfbe541
Shuyang Sun <sunbuffett@gmail.com>
2021-01-17 14:01:57 -0500

    Add conway state character conversion.

--- gol-core/src/cell/state.rs
+++ gol-core/src/cell/state.rs
@@ -3,3 +3,24 @@ pub enum ConwayState {
     Alive,
     Dead,
 }
+
+const CONWAY_STATE_ALIVE_CHAR: char = 'x';
+const CONWAY_STATE_DEAD_CHAR: char = ' ';
+
+impl std::convert::Into<char> for ConwayState {
+    fn into(self) -> char {
+        match self {
+            ConwayState::Alive => CONWAY_STATE_ALIVE_CHAR,
+            ConwayState::Dead => CONWAY_STATE_DEAD_CHAR,
+        }
+    }
+}
+
+impl std::convert::From<char> for ConwayState {
+    fn from(ch: char) -> Self {
+        match ch {
+            CONWAY_STATE_ALIVE_CHAR => ConwayState::Alive,
+            _ => ConwayState::Dead,
+        }
+    }
+}
