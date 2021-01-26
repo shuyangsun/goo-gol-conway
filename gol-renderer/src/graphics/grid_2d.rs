f059ff87388a2c608f3c5e80573611f6071e868c
Shuyang Sun <sunbuffett@gmail.com>
2021-01-25 19:10:38 -0500

    Setup callback impl for graphical renderer 2d.

--- gol-renderer/src/graphics/grid_2d.rs
+++ gol-renderer/src/graphics/grid_2d.rs
@@ -1,16 +1,36 @@
 use crate::{util::grid_util::find_2d_bounds, ColorMapping};
+use gol_core::{BoardCallback, GridPoint2D, IndexedDataOwned};
+use num_traits::{CheckedSub, FromPrimitive, ToPrimitive};
+use rayon::prelude::*;
+use std::sync::{Arc, Mutex};
 use tokio::sync::broadcast::{error::TryRecvError, Receiver};
 
-pub struct GraphicalRendererGrid2D<M> {
+pub struct GraphicalRendererGrid2D<M, I> {
     title: String,
     iter: usize,
     screen_dim: (u32, u32),
     grid_bounds: Option<(u32, u32, u32, u32)>,
     rx: Option<Receiver<char>>,
     color_map: M,
+    cur_states: Arc<Mutex<Option<I>>>,
 }
 
-impl<M> GraphicalRendererGrid2D<M> {
+impl<T, U, I, M> BoardCallback<T, GridPoint2D<U>, I> for GraphicalRendererGrid2D<M, I>
+where
+    T: Send + Sync + Clone,
+    U: Send + Sync + Clone + Ord + CheckedSub + ToPrimitive + FromPrimitive,
+    I: ParallelIterator<Item = IndexedDataOwned<GridPoint2D<U>, T>>,
+    M: Send + Sync + ColorMapping<T>,
+{
+    fn setup(&mut self) {}
+    fn cleanup(&mut self) {}
+
+    fn execute(&mut self, states: I) {
+        *self.cur_states.lock().unwrap() = Some(states);
+    }
+}
+
+impl<M, I> GraphicalRendererGrid2D<M, I> {
     pub fn new(color_map: M) -> Self {
         Self::new_with_title(color_map, String::from(""))
     }
@@ -23,6 +43,7 @@ impl<M> GraphicalRendererGrid2D<M> {
             grid_bounds: None,
             rx: None,
             color_map,
+            cur_states: Arc::new(Mutex::new(None)),
         }
     }
 
@@ -38,6 +59,7 @@ impl<M> GraphicalRendererGrid2D<M> {
             grid_bounds: None,
             rx: Some(receiver),
             color_map,
+            cur_states: Arc::new(Mutex::new(None)),
         }
     }
 }
