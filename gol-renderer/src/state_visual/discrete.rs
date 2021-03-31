9dc5827e5d3c9d34998c46639a7438c3b2b545be
Shuyang Sun <sunbuffett@gmail.com>
2021-03-31 17:35:34 -0400

    Change color map.

--- gol-renderer/src/state_visual/discrete.rs
+++ gol-renderer/src/state_visual/discrete.rs
@@ -33,14 +33,47 @@ where
             }
         } else {
             let ratio = state.to_f64().unwrap() / (self.state_count() - 1) as f64;
-            let high = (u16::MAX as f64 * ratio).ceil() as u16;
-            let low = (u16::MAX as f64 * (1.0 - ratio)).floor() as u16;
-            RGBA16 {
-                r: low,
-                g: high,
-                b: 0,
-                a: high,
-            }
+            let hue = 1. - ratio;
+            hsl_to_rbg(hue, 1.0, 0.5, ratio)
         }
     }
+}
+
+fn hsl_to_rbg(h: f64, s: f64, l: f64, a: f64) -> RGBA16 {
+    let (mut r, mut g, mut b) = (l, l, l);
+    if s > 0. {
+        let q = if l < 0.5 { l * (1. + s) } else { l + s - l * s };
+        let p = 2. * l - q;
+        r = hue_to_rgb(p, q, h + 1. / 3.);
+        g = hue_to_rgb(p, q, h);
+        b = hue_to_rgb(p, q, h - 1. / 3.);
+    }
+
+    let max = u16::MAX as f64;
+    RGBA16 {
+        r: (r * max) as u16,
+        g: (g * max) as u16,
+        b: (b * max) as u16,
+        a: (a * max) as u16,
+    }
+}
+
+fn hue_to_rgb(p: f64, q: f64, t: f64) -> f64 {
+    let mut t = t;
+    if t < 0. {
+        t += 1.;
+    }
+    if t > 1. {
+        t -= 1.;
+    }
+    if t < 1. / 6. {
+        return p + (q - p) * t * 6.;
+    }
+    if t < 1. / 2. {
+        return q;
+    }
+    if t < 2. / 3. {
+        return p + (q - p) * (2. / 3. - t) * 6.;
+    }
+    return p;
 }
